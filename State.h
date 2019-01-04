44a715bfb36e654a3a3f64bfc4b3e5d7467ad434
IssaR <d0zer807@gmail.com>
2019-01-04 21:37:56 +0200

    State.cpp

---
+++ State.h
@@ -0,0 +1,14 @@
+//
+// Created by issa on 1/4/19.
+//
+
+#ifndef MAILSTONE2_STATE_H
+#define MAILSTONE2_STATE_H
+
+template <class T>
+class State {
+    T state;
+};
+
+
+#endif //MAILSTONE2_STATE_H
